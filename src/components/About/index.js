4eb9b330c4e084e04316b2cbd70c75959fa50d69
Brandon Jimenez <bjimenezp09@gmail.com>
2023-01-23 23:10:09 -0600

    created about index.js file

---
+++ src/components/About/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+
+const AboutMe = () => {
+  return (
+    <MDBContainer className="mt-5">
+      <MDBRow>
+        <MDBCol md="12">
+          <h2>About Me</h2>
+          <hr class="solid bg-dark" />
+          <img
+            style={{ height: "285px" }}
+            src=""
+            class="float-left pr-4 pb-3 img-fluid"
+            alt="Brandon Jimenez"
+          />
+          <p style={{ fontSize: "27px" }}>
+            About
+          </p>
+          <p style={{ fontSize: "27px" }}>
+            Description
+          </p>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
+
+export default AboutMe;
